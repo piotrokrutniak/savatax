5a901528f45798bc059094fde27038e650956047
piotrokrutniak <91792866+piotrokrutniak@users.noreply.github.com>
2023-10-18 19:20:59 +0200

    Lint response fix

--- .history/src/app/api/email/route_20231018191425.ts
+++ .history/src/app/api/email/route_20231018191425.ts
@@ -1,4 +1,11 @@
 import { SendConfirmation, SendEmail } from "@/app/utilities/useGmailApi";
+import { NextApiRequest, NextApiResponse } from "next";
+import bodyParser from "body-parser"
+
+const jsonParser = bodyParser.json();
+type ResponseData = {
+  message: string
+}
  
 export async function POST(request: Request) {
   const data = await request.json()
