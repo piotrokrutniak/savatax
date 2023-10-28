d4dad9206c1ab1af74aabb317c076828ac1664c1
piotrokrutniak <91792866+piotrokrutniak@users.noreply.github.com>
2023-10-29 01:42:56 +0200

    Clean-up

--- next.config.js
+++ next.config.js
@@ -9,14 +9,6 @@ const nextConfig = {
         locales: ['en', 'pl'],
         defaultLocale: 'en',
     },
-    // env:{
-    //     SMTP_HOST: process.env.SMTP_HOST,
-    //     SMTP_PORT: process.env.SMTP_PORT,
-    //     SMTP_USER: process.env.SMTP_USER,
-    //     SMTP_PASSWORD: process.env.SMTP_PASSWORD,
-    //     SMTP_CONTACT: process.env.SMTP_CONTACT,
-    //     SMTP_FORM_INBOX: process.env.SMTP_FORM_INBOX
-    // }
 }
 
 module.exports = nextConfig
